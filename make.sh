d7c9ff2afa9144c23711f86f57b76570b29bd87f
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2020-12-28 17:56:41 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DTS=arch/arm64/boot/dts
+DTBTOOL=tools/dtbtool
 export ARCH=arm64
 export CROSS_COMPILE=/home/neel/Desktop/toolchain/linaro/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
@@ -9,20 +11,16 @@ rm ./arch/arm64/boot/Image
 rm ./arch/arm64/boot/Image.gz
 rm ./arch/arm64/boot/dts/*.dtb
 make clean && make mrproper	
-
 echo "============="
 echo "Building DTB"
 echo "============="
-DTS=arch/arm64/boot/dts
-chmod a+x ./tools/dtbtool 
 make exynos7870-j7velte_sea_open_00.dtb exynos7870-j7velte_sea_open_01.dtb exynos7870-j7velte_sea_open_03.dtb
-./tools/dtbtool $DTS/ -o ./dtb
+./$DTBTOOL $DTS/ -o ./dtb
 echo "============="
 echo "Building Zimg"
 echo "============="
 #make j6lte_defconfig
 make j7velte_defconfig
-make -j3
-echo "Done"
-echo ""
+make -j$(($(nproc)+1))
+echo -e "Done\n"
 cp -r ./arch/arm64/boot/Image ./boot.img-zImage
